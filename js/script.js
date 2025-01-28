057f5b7038a92641b36741c6e5578f4255505628
KirillPastushenko <kpastushenko@gmail.com>
2025-01-28 16:41:47 +0300

    сжаты картинки \ добавлена адаптивность

--- js/script.js
+++ js/script.js
@@ -26,22 +26,51 @@ $(document).ready(function(){
    $('h1 span').addClass('reveal-text');
    $('.top-pic').addClass('fromLeft');
 
-   $('.btn-paint').on('click', function(){
-      $('.paint-wrapper, .paint-poster1, .paint-poster2').toggleClass('active')
+   $('.btn-paint').on('click', function(e){
+      const isActive = $(this).hasClass('active');
+      const isPdf = $(this).hasClass('btn-orange'); 
+      const btnText = isActive ? 'Хочу творить!' : 'Завершить';
+
+      if (isPdf) return;
+
+      e.preventDefault();
+      
+      $('.btn-paint span').text(btnText);
+
+      $('.paint-wrapper, .paint-poster1, .paint-poster2, .btn-paint').toggleClass('active');
    })
 
    $('.paint-palitra-item').on('click', function(){
-      $('#buttons .button').removeClass('clickable');
-      $('.paint-palitra-item').removeClass('active');
-      const id = $(this).attr('data-id')
-      $(this).addClass('active');
+      if(!$(this).hasClass('disable')) {
+         const id = $(this).attr('data-id');
+         $('#buttons .button').removeClass('clickable');
+         $(`.button.${id}`).addClass('clickable');
+         $('#paint').addClass('with-brush');
 
-      $(`.button.${id}`).addClass('clickable');
-
+         $(this).addClass('active');
+      }
    })
 
    $('#buttons .button').on('click', function(){
-      $(this).addClass('active');
-      $(this).removeClass('clickable');
+      if(!$(this).hasClass('disable')) {
+         const id = $(this).attr('data-id')
+
+         $(this).addClass('active').addClass('disable');
+         $(this).removeClass('clickable');
+         $('#paint').removeClass('with-brush');
+         $(`.paint-palitra-item[data-id="${id}"]`)
+            .removeClass('active')
+            .addClass('disable');
+
+         if($('.paint-palitra-item.disable').length === 6) {
+            $('.btn-paint')
+               .attr({'href':'http://pdf.pdf', '_target': 'blank'})
+               .addClass('btn-orange')
+               .removeClass('active');
+            
+            $('.btn-paint span').text('Хочу скачать!');
+            $('.btn-paint img').attr('src', 'img/btn-pdf.png');
+         }
+      }
    })
 })
